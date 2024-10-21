da7e735a483947d275b1b2ff49ff8cf5c738cf88
jarndt <jason@codestreaminc.com>
2024-10-20 20:05:17 -0600

    CHORE: remove legacy storefront overrides

--- app/overrides/add_digital_downloads_to_invoice.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Deface::Override.new(
-  virtual_path: "spree/shared/_order_details",
-  name: "add_digital_downloads_to_invoice",
-  insert_bottom: "td[data-hook='order_item_description']",
-  partial: "spree/shared/digital_download_links"
-)
