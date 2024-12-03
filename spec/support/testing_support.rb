5c803749cbbed34a0e05cf130a0823fdaf676555
jarndt <jason@codestreaminc.com>
2024-12-03 13:11:20 -0700

    Fix Style/StringConcatenation cop
    Style/StringConcatenation: Prefer string interpolation to string concatenation.
            File.open(SolidusDigital::Engine.root + "spec/fixtures" + filename)

--- spec/support/testing_support.rb
+++ spec/support/testing_support.rb
@@ -4,7 +4,7 @@ module SolidusDigital
   module TestingSupport
     module Helpers
       def image(filename)
-        File.open(SolidusDigital::Engine.root + "spec/fixtures" + filename)
+        File.open("#{SolidusDigital::Engine.root}/spec/fixtures/#{filename}")
       end
 
       def upload_image(filename)
